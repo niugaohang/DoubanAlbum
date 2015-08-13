80ee47c67b7069ab0fd506d9cbf220ddd971fc07
牛高航 <niugaohang@niugaohangdeiMac.local>
2015-08-13 15:25:50 +0800

    Initial Commit

---
+++ DoubanAlbum/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoubanAlbum
+//
+//  Created by 牛高航 on 15/8/13.
+//  Copyright (c) 2015年 牛高航. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
